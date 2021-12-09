880c139f4b2247bf1a76275d026475c3ddfded2b
Victor Tihu <tihuvictor@gmail.com>
2021-12-09 22:34:05 +0200

    Add project files.

---
+++ write_or_writeline/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace write_or_writeline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello ");
+            Console.Write("World");
+            Console.Write("!!!");
+            Console.ReadLine();
+        }
+    }
+}
